286cf1b5f3911d928146d368267653d5f5b562bc
kahraba <Ahmedibrahim1310@icloud.com>
2021-03-04 17:42:20 +0200

    hh

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:almosawii/secreens/splashscreen.dart';
 import 'package:almosawii/secreens/wrapper/wrapper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
