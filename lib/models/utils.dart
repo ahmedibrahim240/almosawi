4a1fa422aa7728052600825afc878e7bea19738f
kahraba <Ahmedibrahim1310@icloud.com>
2021-04-10 10:14:42 +0200

    add utils

---
+++ lib/models/utils.dart
@@ -0,0 +1,6 @@
+class Utils {
+  // ignore: non_constant_identifier_names
+  static final BASE_URL = "https://ahmadalmosawi.com/V2/api/";
+  // ignore: non_constant_identifier_names
+  static final Register_URL = "Register";
+}
