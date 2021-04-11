39c183b65c660662ca9890216b077cca7383f5e6
kahraba <Ahmedibrahim1310@icloud.com>
2021-04-11 19:58:24 +0200

    git home video

---
+++ lib/models/homeVideoApi.dart
@@ -0,0 +1,43 @@
+import 'package:almosawii/models/utils.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomheVideoModels {
+  final String homeVideo;
+  final String homeVideoTitle;
+  final String homeVideoTxt1;
+  final String homeVideoTxt2;
+  final String homeVideoTxt3;
+
+  HomheVideoModels({
+    this.homeVideo,
+    this.homeVideoTitle,
+    this.homeVideoTxt1,
+    this.homeVideoTxt2,
+    this.homeVideoTxt3,
+  });
+}
+
+class HomeVideoApi {
+  static Future<HomheVideoModels> futchHomeVideo() async {
+    HomheVideoModels aboutUs;
+    try {
+      var response = await http.get(Utils.GeneralData_URL);
+      var jsonData = json.decode(response.body);
+      print('response.statusCode:${response.statusCode}');
+      if (response.statusCode == 200) {
+        aboutUs = HomheVideoModels(
+          homeVideo: jsonData['data']['HomeVideo'],
+          homeVideoTitle: jsonData['data']['HomeVideoTitle'],
+          homeVideoTxt1: jsonData['data']['HomeVideoTxt1'],
+          homeVideoTxt2: jsonData['data']['HomeVideoTxt2'],
+          homeVideoTxt3: jsonData['data']['HomeVideoTxt3'],
+        );
+      }
+    } catch (e) {
+      print('contatus Errrrrrrrrror');
+      print(e);
+    }
+    return aboutUs;
+  }
+}
--- lib/secreens/home/homeVideo.dart
+++ lib/secreens/home/homeVideo.dart
@@ -1,6 +1,7 @@
 import 'package:almosawii/constants/themes.dart';
+import 'package:almosawii/models/homeVideoApi.dart';
 import 'package:flutter/material.dart';
-// import 'package:vimeoplayer_trinity/vimeoplayer_trinity.dart';
+import 'package:vimeoplayer/vimeoplayer.dart';
 
 class HomeVideo extends StatefulWidget {
   @override
@@ -8,132 +9,102 @@ class HomeVideo extends StatefulWidget {
 }
 
 class _HomeVideoState extends State<HomeVideo> {
-  String conant =
-      'محاضرة مجانية، طريقك للنجاح وتطوير عملك في مجال التداول والفوركس مع أحمد الموسوي';
-  List<String> rowText = [
-    'تمنحك بداية صحيحة وتتعرف على مكان الخلل في طريقة التداول',
-    'تستفيد من أخطائي في التداول',
-    'تمدك بكل ما تحتاجه لتصبح متداولا محترفا، وتمكنك من جني أرباح من سوق التداول انطلاقا من تجربتي الخاصة',
-  ];
-  List ides = ['395212534', '528749215', '509764916'];
-  int index = 0;
-  String id;
-  @override
-  void initState() {
-    if (index == 0) {
-      id = ides[index];
-    } else {
-      addVideo();
-    }
-    super.initState();
-  }
-
-  addVideo() {
-    print('length:${ides.length}');
-    setState(() {
-      index = index + 1;
-    });
-
-    if (index >= ides.length) {
-      setState(() {
-        index = 0;
-        id = ides[index];
-      });
-    } else {
-      setState(() {
-        id = ides[index];
-      });
-    }
-    print('id:$id');
-    print('index:$index');
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(
-            'lib/images/homeback.jpg',
-          ),
-          fit: BoxFit.fitHeight,
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 300,
-            child: Column(
-              children: [
-                // VimeoPlayer(id: id, autoPlay: false),
-                RaisedButton(
-                  onPressed: () {
-                    setState(() {
-                      addVideo();
-                    });
-                  },
-                  child: Text('add video'),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            color: Colors.black.withOpacity(.5),
-            child: ListView(
-              shrinkWrap: true,
-              primary: false,
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              children: [
-                Text(
-                  conant,
-                  style: AppTheme.subHeading.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w800,
-                  ),
-                ),
-                ListView.builder(
-                  shrinkWrap: true,
-                  primary: false,
-                  itemCount: rowText.length,
-                  itemBuilder: (context, index) {
-                    return Column(
-                      children: [
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.check,
-                              color: Colors.white,
-                            ),
-                            SizedBox(width: 5),
-                            SizedBox(
-                              width: MediaQuery.of(context).size.width - 100,
-                              child: Text(
-                                rowText[index],
-                                style: AppTheme.subHeading.copyWith(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w800,
-                                ),
-                              ),
-                            ),
-                          ],
+    return FutureBuilder(
+      future: HomeVideoApi.futchHomeVideo(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          print(snapshot.data);
+          return (snapshot.data == null)
+              ? Container()
+              : (snapshot.data.homeVideoTitle == '' ||
+                      snapshot.data.homeVideoTitle == '')
+                  ? Container()
+                  : Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                            'lib/images/homeback.jpg',
+                          ),
+                          fit: BoxFit.fitHeight,
                         ),
-                        SizedBox(height: 5),
-                      ],
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          (snapshot.data.homeVideo == '')
+                              ? Container()
+                              : VimeoPlayer(
+                                  id: snapshot.data.homeVideo,
+                                  autoPlay: false,
+                                ),
+                          Container(
+                            color: Colors.black.withOpacity(.5),
+                            child: ListView(
+                              shrinkWrap: true,
+                              primary: false,
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 10),
+                              children: [
+                                Text(
+                                  snapshot.data.homeVideoTitle,
+                                  style: AppTheme.subHeading.copyWith(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w800,
+                                  ),
+                                ),
+                                (snapshot.data.homeVideoTxt1 == '')
+                                    ? Container()
+                                    : rowTexts(
+                                        context: context,
+                                        title: snapshot.data.homeVideoTxt1,
+                                      ),
+                                (snapshot.data.homeVideoTxt2 == '')
+                                    ? Container()
+                                    : rowTexts(
+                                        context: context,
+                                        title: snapshot.data.homeVideoTxt2,
+                                      ),
+                                (snapshot.data.homeVideoTxt3 == '')
+                                    ? Container()
+                                    : rowTexts(
+                                        context: context,
+                                        title: snapshot.data.homeVideoTxt3,
+                                      ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     );
-                  },
-                ),
-              ],
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+
+  rowTexts({BuildContext context, String title}) {
+    return Row(
+      children: [
+        Icon(
+          Icons.check,
+          color: Colors.white,
+        ),
+        SizedBox(width: 5),
+        SizedBox(
+          width: MediaQuery.of(context).size.width - 100,
+          child: Text(
+            title,
+            style: AppTheme.subHeading.copyWith(
+              color: Colors.white,
+              fontWeight: FontWeight.w800,
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
