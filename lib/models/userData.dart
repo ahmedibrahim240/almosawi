10498e28984b63c09c2d6afa5403ec814819a237
kahraba <Ahmedibrahim1310@icloud.com>
2021-04-27 19:22:00 +0200

    remove some class

--- lib/models/userData.dart
+++ lib/models/userData.dart
@@ -1,20 +1,3 @@
-class UserDate {
-  final String userName;
-  final String userEmail;
-  final String userImage;
-  final String userPhoneNamber;
-
-  UserDate(
-      {this.userName, this.userEmail, this.userImage, this.userPhoneNamber});
-}
-
-UserDate userDate = UserDate(
-  userName: 'A.Kahraba',
-  userImage: 'lib/images/user.jpg',
-  userEmail: 'AhmedIbrahim1310@ilcoud.com',
-  userPhoneNamber: '0155105150',
-);
-
 class User {
   static int userid;
   static String proChat;
@@ -31,10 +14,4 @@ class User {
   static bool userSkipLogIn;
   static bool userBuyPlan;
   static bool userCantBuy;
-}
-
-class UserToken {
-  final String userTohen;
-
-  UserToken({this.userTohen});
 }
--- lib/secreens/archives/archives.dart
+++ lib/secreens/archives/archives.dart
@@ -1,8 +1,7 @@
 import 'package:almosawii/constants/constans.dart';
 import 'package:almosawii/constants/themes.dart';
 import 'package:almosawii/models/RecomendationsApi.dart';
-import 'package:almosawii/models/recommendModels.dart';
-import 'package:almosawii/secreens/Recommendations/recommendations.dart';
+
 import 'package:almosawii/secreens/archives/archiveRecoPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -185,66 +184,6 @@ class _ArchivesState extends State<Archives> {
                   }
                 },
               ),
-      ),
-    );
-  }
-
-  gardViewOfAllCourses({BuildContext context}) {
-    return GridView.count(
-      crossAxisCount: 1,
-      primary: false,
-      childAspectRatio: 1,
-      shrinkWrap: true,
-      children: List.generate(
-        recommendList.length,
-        (index) {
-          return Column(
-            children: [
-              Row(
-                children: [
-                  Text(
-                    recommendList[index].title,
-                    style: AppTheme.subHeading,
-                  ),
-                  Text(
-                    '(${recommendList[index].stockNumber})',
-                    style: AppTheme.subHeading,
-                  ),
-                  Text(
-                    (recommendList[index].stockRatio < 0)
-                        ? '${recommendList[index].stockRatio}%'
-                        : '+${recommendList[index].stockRatio}%',
-                    style: AppTheme.subHeading.copyWith(
-                      color: (recommendList[index].stockRatio < 0)
-                          ? Colors.red
-                          : customColor,
-                    ),
-                  ),
-                ],
-              ),
-              Container(
-                height: 200,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('lib/images/graph.jpeg'),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              CustomButton(
-                onPress: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (_) => Recommendations(),
-                    ),
-                  );
-                },
-                text: 'عرض نصائح أستاذ احمد',
-              ),
-            ],
-          );
-        },
       ),
     );
   }
